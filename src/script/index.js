2c95695d2d4f270ffbb1e73f228e963b1beb8e38
Ira Redchets <irared95@gmail.com>
2022-08-10 15:46:11 +0300

    removed spaces and console.log

--- src/script/index.js
+++ src/script/index.js
@@ -15,21 +15,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 } else {
                     renderNoFilms()
                 }
-
-
             })
     }
 
-   function popularFilmsApi() {
-       fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-           .then((response) => response.json())
-           .then((myJson) => {
-               deleteFilms()
-               deleteTextNoFilms()
-               renderFilms(myJson.results)
-           })
-   }
-
+    function popularFilmsApi() {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+            .then((response) => response.json())
+            .then((myJson) => {
+                deleteFilms()
+                deleteTextNoFilms()
+                renderFilms(myJson.results)
+            })
+    }
     popularFilmsApi()
 
     const buttonSearch = document.querySelector('.search__button--js')
@@ -44,7 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
             event.preventDefault();
             searchFilmsApi()
         }
-
     });
 
     const addZero = (number) => number < 10 ? `0${number}` : number
@@ -56,26 +52,28 @@ document.addEventListener('DOMContentLoaded', function () {
         let yyyy = dateValue.getFullYear();
         return `${addZero(dd)}.${addZero(mm)}.${yyyy}`
     }
-    const iconFavorites = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 117.42"><path d="M66.71 3.55L81.1 37.26l36.58 3.28v-.01c1.55.13 2.91.89 3.85 2.01a5.663 5.663 0 011.32 4.13v.01a5.673 5.673 0 01-1.69 3.57c-.12.13-.25.25-.39.36L93.25 74.64l8.19 35.83c.35 1.53.05 3.06-.73 4.29a5.652 5.652 0 01-3.54 2.52l-.14.03c-.71.14-1.43.15-2.12.02v.01c-.75-.13-1.47-.42-2.11-.84l-.05-.03-31.3-18.71-31.55 18.86a5.664 5.664 0 01-7.79-1.96c-.38-.64-.62-1.33-.73-2.02-.1-.63-.09-1.27.02-1.89.02-.13.04-.27.08-.4l8.16-35.7c-9.24-8.07-18.74-16.1-27.83-24.3l-.08-.08a5.64 5.64 0 01-1.72-3.7c-.1-1.45.36-2.93 1.4-4.12l.12-.13.08-.08a5.668 5.668 0 013.77-1.72h.06l36.34-3.26 14.44-33.8c.61-1.44 1.76-2.5 3.11-3.05 1.35-.54 2.9-.57 4.34.04.69.29 1.3.71 1.8 1.22.53.53.94 1.15 1.22 1.82l.02.06zm10.19 37.2L61.85 5.51a.42.42 0 00-.09-.14.42.42 0 00-.14-.09.427.427 0 00-.35 0c-.1.04-.19.12-.24.24L45.98 40.75c-.37.86-1.18 1.49-2.18 1.58l-37.9 3.4c-.08.01-.16.02-.24.02-.06 0-.13.02-.18.05-.03.01-.05.03-.07.05l-.1.12c-.05.08-.07.17-.06.26.01.09.04.18.09.25.06.05.13.11.19.17l28.63 25c.77.61 1.17 1.62.94 2.65l-8.51 37.22-.03.14c-.01.06-.02.12-.01.17a.454.454 0 00.33.36c.12.03.24.02.34-.04l32.85-19.64c.8-.5 1.85-.54 2.72-.02L95.43 112c.08.04.16.09.24.14.05.03.1.05.16.06v.01c.04.01.09.01.14 0l.04-.01c.12-.03.22-.1.28-.2.06-.09.08-.21.05-.33L87.8 74.28a2.6 2.6 0 01.83-2.55l28.86-25.2c.04-.03.07-.08.1-.13.02-.04.03-.1.04-.17a.497.497 0 00-.09-.33.48.48 0 00-.3-.15v-.01c-.01 0-.03 0-.03-.01l-37.97-3.41c-1-.01-1.93-.6-2.34-1.57z" fill="#ffcf00"/></svg> '
-    const templateFilmsList = (title, overview, releaseDate, poster, rating) => `<li class="films__item films__item--js"> <span class="films__rating">Rating:${rating}</span> <span class="films__release">${releaseDate}</span> <div class="card card--js"> <div class="card__font"><img src="${poster}" alt="${title}"></div> <div class="card__back card__back--js">  <p class="card__favorites">${iconFavorites}</p><p class="card__content">${overview}</p> </div> </div> <p class="films__name">${title}</p>   </li>`
 
+    const iconFavorites = '<svg height="24px" viewBox="0 0 24 24" width="24px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg""><path d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521    c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506    c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625    c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191    s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586    s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z"/></svg>'
+    const templateFilmsList = (id, title, overview, releaseDate, poster, rating, existFilmInFavorites) => `<li class="films__item films__item--js" data-id="${id}"> <span class="films__rating">Rating:${rating}</span> <span class="films__release">${releaseDate}</span> <div class="card card--js" > <div class="card__front"><img src="${poster}" alt="${title}"></div> <div class="card__back card__back--js"><div class="card__inner"><p class="card__favorites ${existFilmInFavorites ? 'active' : ''} ">${iconFavorites}</p><p class="card__content">${overview}</p></div></div> </div> <p class="films__name">${title}</p>   </li>`
     const rootFilmsList = document.querySelector('.films__list--js')
 
     function renderFilms(filmsItems) {
+        const favoritesFilms = store.favoritesFilms
         filmsItems.forEach(filmsItem => {
             const title = filmsItem.title
             const overview = filmsItem.overview
             const releaseDate = formatToDDMMYYYY(filmsItem.release_date)
             const poster = filmsItem.poster_path ? `${PATH_IMG}${filmsItem.poster_path}` : PLACEHOLDER_NO_IMAGE
             const rating = filmsItem.vote_average
-            const templateFilms = templateFilmsList(title, overview, releaseDate, poster, rating)
+            const id = filmsItem.id
+            //сущесвутет ли айди в лайкнутых фильмах
+            const existFilmInFavorites =  favoritesFilms.includes(id.toString())
+            const templateFilms = templateFilmsList(id, title, overview, releaseDate, poster, rating, existFilmInFavorites)
             rootFilmsList.innerHTML += templateFilms
         })
     }
 
-
     const templateNoFilms = (text) => `<p class="empty-films empty-films--js">${text}</p>`
-
     const rootNoFilms = document.querySelector('.root-empty-films--js')
 
     function renderNoFilms() {
@@ -83,45 +81,47 @@ document.addEventListener('DOMContentLoaded', function () {
         rootNoFilms.innerHTML += renderedNoFilms
     }
 
-
     function deleteFilms() {
         const filmsItems = document.querySelectorAll('.films__item--js')
         if (filmsItems.length) {
             filmsItems.forEach(filmsItem => {
                 filmsItem.remove()
             })
-
         }
     }
 
-    function deleteTextNoFilms(){
+    function deleteTextNoFilms() {
         const deleteText = document.querySelector('.empty-films--js')
         if (deleteText) deleteText.remove()
-
     }
-    ////
-    const btnFavorites = document.querySelectorAll('.card--js')
-    document.addEventListener('click',function(e){
-        if(e.target.className === 'card__favorites'){
-            console.log(111111)
+
+    document.addEventListener('click', function (e) {
+        if (e.target.className.includes('card__favorites')) {
+            const id = e.target.closest('.films__item--js').getAttribute('data-id')
+            addFavorites(id)
+            //закрашивание звездочки
+            e.target.classList.toggle('active')
         }
-    });
-
-
-    btnFavorites.forEach(function (item,index){
-        console.log(333)
-        item.addEventListener('click',function (e){
-            console.log(222)
-        })
     })
 
-    // const storage = window.localStorage
-    // const user = ['ira', 'john']
-    //
-    // storage.setItem('user111', JSON.stringify(user))
-
-
-
+    const store = {
+        favoritesFilms: JSON.parse(localStorage.getItem('favoritesFilms')) || [] //получаем фильмы
+    }
+   //срабатывает при клике
+    function addFavorites(id) {
+        const favoritesFilms = store.favoritesFilms   //деструктуризация иначе store.films
+        const existFilm = favoritesFilms.includes(id) //содержит ли массив лайкнутое айди, возвращает true или false.
+        if (!existFilm){ // если нет
+            favoritesFilms.push(id) // то его добавляем в массив
+        } else{
+           // иначе удаляем с массива
+            const findFilmIndex = favoritesFilms.findIndex(filmId => {
+                return filmId === id
+            })
+            favoritesFilms.splice(findFilmIndex, 1 )
+        }
+        localStorage.setItem('favoritesFilms', JSON.stringify(favoritesFilms)); // сохранение в браузере выбранных фильмов
+    }
 
 })
 
